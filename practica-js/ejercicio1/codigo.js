0dedfe833cd034de75175b593148da9d595b307a
vicmoritan <vicmoritan@gmail.com>
2026-05-17 23:29:41 -0300

    ejercicio 1 js

---
+++ practica-js/ejercicio1/codigo.js
@@ -0,0 +1,9 @@
+//a) muestra un mensaje que dice "hola mundo" al cargar la pág
+alert("Hola Mundo!");
+
+//b) luego del primer mensaje, muestra un mensaje que dice "soy el primer script"
+alert("Soy el primer script");
+
+//e) y f), mostrar mensaje almacenado en una variable. \ sirve para comillas dobles, \n para salto de línea
+mensaje = "Hola mundo! Que fácil es incluir \n 'comillas simples' \n y \n \"comillas dobles\" \n "
+alert(mensaje);
