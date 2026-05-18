8ebf81db45bd40b946dc7bda3417d8e16b1e8299
vicmoritan <vicmoritan@gmail.com>
2026-05-17 23:39:43 -0300

    ejercicio 2 js

---
+++ practica-js/ejercicio2/script.js
@@ -0,0 +1,6 @@
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+// mostrar 12 meses en pantalla, uno por uno
+for(let i = 0; i < meses.length; i++ ){
+    alert(meses[i]);
+}
